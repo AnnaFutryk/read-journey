84eec0d2673b02f9e4b2af951fbbd69f6214abad
AnnaFutryk <anna.futryk@gmail.com>
2024-01-20 00:41:50 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}></Route>
+      <Route path="/" element={<Layout />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      {/* <Route
+        path="register"
+        element={
+          <RestrictedRoute redirectTo="/contacts">
+            <RegisterPage />
+          </RestrictedRoute>
+        }
+      /> */}
     </Routes>
   );
 }
