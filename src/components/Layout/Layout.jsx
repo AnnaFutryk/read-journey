e4e431dda4a281c82389abdc1a0d40f669d60f90
AnnaFutryk <anna.futryk@gmail.com>
2024-01-20 00:41:11 +0200

    add layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
-import Loader from "../../components/Loader/Loader";
+import Loader from "../common/Loader/Loader";
 
 const Layout = () => {
   return (
--- src/components/Loader/Loader.styled.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const LoderWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-top: 80px;
-`;
