d4f50d256a555955e0adfacf5605608fd62ca962
AnnaFutryk <anna.futryk@gmail.com>
2024-01-22 02:10:53 +0200

    update auth form component

---
+++ src/components/modules/AuthSection/AuthForm/AuthForm.jsx
@@ -0,0 +1,175 @@
+import { ErrorMessage, Formik } from "formik";
+import { ToastContainer, toast, Slide } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useState } from "react";
+import * as Yup from "yup";
+import {
+  Input,
+  InputWrapper,
+  AuthForm,
+  ToggleButton,
+  ErrorText,
+  ErrorSvgStyled,
+  CheckSvgStyled,
+  SuccessTxt,
+  Link,
+  BtnsBlock,
+  Label,
+  BtnContainer,
+} from "./AuthForm.styled";
+import { ReactComponent as ShowPassword } from "../../../../assets/icons/eye.svg";
+import { ReactComponent as HidePassword } from "../../../../assets/icons/eye-off.svg";
+import WhiteBtn from "../../../common/Button/WhiteBtn/WhiteBtn";
+import { useDispatch } from "react-redux";
+import { authOperations } from "../../../../redux/auth/authOperations";
+
+const initialValues = { name: "", email: "", password: "" };
+const schema = Yup.object().shape({
+  name: Yup.string()
+    .required("Name is required")
+    .matches(/[A-Za-z]+/, "Name must contain at least one letter")
+    .matches(/^[A-Za-z\s]+$/, "Name must contain only letters and spaces"),
+  email: Yup.string()
+    .matches(
+      /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+      "Emails: digits, letters, . - _ only, e.g., example@mail.com."
+    )
+    .email("Invalid email format, example@mail.com")
+    .required("Email is required"),
+  password: Yup.string()
+    .min(7, "Password must be at least 7 characters long")
+    .max(30, "Passwors must be shorter")
+    .required("Password is required")
+    .matches(/[a-zA-Z]/, "Password must contain at least one letter")
+    .matches(/[0-9]/, "Password must contain at least one number"),
+});
+
+export default function AuthUserForm() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleSubmit = async (values) => {
+    const { name, email, password } = values;
+    try {
+      await dispatch(authOperations.signUp({ name, email, password })).unwrap();
+      toast.success(`🦄 Your sign up was successful!`, {
+        position: "top-right",
+        autoClose: 1500,
+      });
+    } catch (error) {
+      toast.error(`Something went wrong. Try again`, {
+        position: "top-right",
+        autoClose: 1500,
+      });
+    }
+  };
+
+  return (
+    <>
+      <ToastContainer transition={Slide} />
+
+      <Formik
+        initialValues={initialValues}
+        validationSchema={schema}
+        onSubmit={handleSubmit}
+      >
+        {({ values, errors, touched }) => (
+          <AuthForm>
+            <>
+              <InputWrapper>
+                <Label htmlFor="name">Name:</Label>
+                <Input
+                  style={{
+                    paddingLeft: "65px",
+                  }}
+                  type="text"
+                  name="name"
+                  placeholder="Ilona Ratushniak"
+                  error={errors.name && touched.name ? "true" : "false"}
+                  success={values.name && !errors.name ? "true" : "false"}
+                />
+                <ErrorMessage
+                  name="name"
+                  render={(message) => <ErrorText>{message}</ErrorText>}
+                />
+                {errors.name && touched.name ? (
+                  <ErrorSvgStyled />
+                ) : values.name && !errors.name ? (
+                  <CheckSvgStyled />
+                ) : null}
+              </InputWrapper>
+
+              <InputWrapper>
+                <Label htmlFor="email">Mail:</Label>
+                <Input
+                  style={{ paddingLeft: "53px" }}
+                  type="email"
+                  name="email"
+                  placeholder="Your@email.com"
+                  error={errors.email && touched.email ? "true" : "false"}
+                  success={values.email && !errors.email ? "true" : "false"}
+                />
+                <ErrorMessage
+                  name="email"
+                  render={(message) => <ErrorText>{message}</ErrorText>}
+                />
+                {errors.email && touched.email ? (
+                  <ErrorSvgStyled />
+                ) : values.email && !errors.email ? (
+                  <CheckSvgStyled />
+                ) : null}
+              </InputWrapper>
+
+              <InputWrapper>
+                <Label htmlFor="password">Password:</Label>
+                <Input
+                  style={{
+                    paddingLeft: "86px",
+                  }}
+                  type={showPassword ? "text" : "password"}
+                  value={values.password}
+                  name="password"
+                  placeholder="Yourpasswordhere"
+                  error={errors.password && touched.password ? "true" : "false"}
+                  success={
+                    values.password && !errors.password ? "true" : "false"
+                  }
+                />
+                <ErrorMessage
+                  name="password"
+                  render={(message) => <ErrorText>{message}</ErrorText>}
+                />
+                {errors.password && touched.password && !values.password && (
+                  <ErrorSvgStyled />
+                )}
+                {values.password && !errors.password ? (
+                  <SuccessTxt>Password is secure</SuccessTxt>
+                ) : null}
+                <ToggleButton type="button" onClick={handleTogglePassword}>
+                  {values.password ? (
+                    showPassword ? (
+                      <ShowPassword />
+                    ) : (
+                      <HidePassword />
+                    )
+                  ) : null}
+                </ToggleButton>
+              </InputWrapper>
+            </>
+            <BtnsBlock>
+              <BtnContainer>
+                <WhiteBtn text={"Registration"} />
+              </BtnContainer>
+              <Link to="/login">Already have an account?</Link>
+            </BtnsBlock>
+          </AuthForm>
+        )}
+      </Formik>
+    </>
+  );
+}
---
+++ src/components/modules/AuthSection/AuthForm/AuthForm.styled.js
@@ -0,0 +1,173 @@
+import { Form, Field } from "formik";
+import styled, { css } from "styled-components";
+import { ReactComponent as ErrorSvg } from "../../../../assets/icons/error.svg";
+import { ReactComponent as CheckSvg } from "../../../../assets/icons/check.svg";
+import { NavLink } from "react-router-dom";
+
+export const AuthForm = styled(Form)`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  width: 100%;
+
+  @media (min-width: 768px) {
+    gap: 14px;
+  }
+`;
+
+export const Input = styled(Field)`
+  width: 100%;
+  display: block;
+  padding: 14px;
+  border-radius: 12px;
+  border: none;
+  outline: none;
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 1.33;
+  letter-spacing: -0.24px;
+  border: 1px solid transparent;
+  color: var(--white-color);
+  background-color: var(--deep-gray-color);
+  transition: border var(--transition);
+
+  @media (min-width: 768px) {
+    padding: 16px 14px;
+    font-size: 14px;
+    line-height: 1.28;
+    letter-spacing: -0.28px;
+  }
+
+  &:focus,
+  &:hover {
+    border: 1px solid rgba(249, 249, 249, 0.1);
+  }
+
+  &::placeholder {
+    color: var(--white-color);
+  }
+
+  ${({ error }) =>
+    error === "true" &&
+    css`
+      border: 1px solid var(--red-color);
+    `}
+  ${({ success }) =>
+    success === "true" &&
+    css`
+      border: 1px solid var(--green-color);
+    `} /* @media (max-width: 768px) {
+    width: 240px;
+    padding: 12px; */
+  /* } */
+`;
+
+export const InputWrapper = styled.div`
+  position: relative;
+`;
+
+export const ToggleButton = styled.button`
+  background-color: transparent;
+  width: 18px;
+  height: 18px;
+  z-index: 10;
+  border: none;
+  position: absolute;
+  top: 13px;
+  right: 16px;
+  padding: 0;
+
+  @media (min-width: 768px) {
+    width: 20px;
+    height: 20px;
+    top: 15px;
+    right: 18px;
+  }
+`;
+
+export const ErrorText = styled.p`
+  position: ${({ absolute, select }) =>
+    absolute === "true" || select === "true" ? "absolute" : "static"};
+  left: ${({ select }) => (select === "true" ? "unset" : "14px")};
+  right: ${({ select }) => (select === "true" ? "0" : "unset")};
+  bottom: -24px;
+  color: var(--red-color);
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 1.16;
+  margin-left: ${({ absolute, select }) =>
+    absolute === "true" || select === "true" ? "0" : "14px"};
+  margin-top: ${({ absolute, select }) =>
+    absolute === "true" || select === "true" ? "0" : "8px"};
+  white-space: nowrap;
+  text-align: ${({ select }) => (select === "true" ? "right" : "left")};
+`;
+
+ErrorText.shouldForwardProp = (prop) => prop !== "error";
+
+export const SuccessTxt = styled.p`
+  color: var(--green-color);
+  font-size: 12px;
+  margin-left: 14px;
+  margin-top: 8px;
+`;
+
+export const ErrorSvgStyled = styled(ErrorSvg)`
+  position: absolute;
+  top: 15px;
+  right: 18px;
+`;
+
+export const CheckSvgStyled = styled(CheckSvg)`
+  position: absolute;
+  top: 15px;
+  right: 18px;
+  font-weight: 500;
+  line-height: 1.16;
+`;
+
+export const BtnsBlock = styled.div`
+  display: flex;
+  gap: 14px;
+  align-items: center;
+  margin-top: 12px;
+
+  @media (min-width: 768px) {
+    margin-top: 68px;
+    gap: 20px;
+  }
+`;
+
+export const Link = styled(NavLink)`
+  color: var(--light-gray-color);
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 1.16;
+  letter-spacing: -0.24px;
+  text-decoration-line: underline;
+
+  @media (min-width: 768px) {
+    font-size: 14px;
+    line-height: 1.28;
+    letter-spacing: -0.28px;
+  }
+`;
+
+export const Label = styled.label`
+  position: absolute;
+  top: 16px;
+  left: 14px;
+  color: var(--light-gray-color);
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 1.285;
+  letter-spacing: -0.28px;
+`;
+
+export const BtnContainer = styled.div`
+  width: 140px;
+
+  @media (min-width: 768px) {
+    width: 225px;
+  }
+`;
