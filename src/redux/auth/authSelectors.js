26f0db88fe286b40adf31a10d3030399a37a7f29
AnnaFutryk <anna.futryk@gmail.com>
2024-01-25 00:31:16 +0200

    Update authSelectors.js

--- src/redux/auth/authSelectors.js
+++ src/redux/auth/authSelectors.js
@@ -1,19 +1,14 @@
-const selectUser = (state) => state.auth.user;
-const selectUserName = (state) => state.auth.user.name;
-const selectAvatarURL = (state) => state.auth.user.avatarURL;
+const selectUserName = (state) => state.auth.name;
+const selectAvatarURL = (state) => state.auth.avatarURL;
 const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
 const selectIsRefreshing = (state) => state.auth.isRefreshing;
 const selectToken = (state) => state.auth.token;
 
-const selectUserAuth = (state) => state.auth.user.userAuth;
-
 const authSelectors = {
-  selectUser,
   selectUserName,
   selectAvatarURL,
   selectToken,
   selectIsLoggedIn,
-  selectUserAuth,
   selectIsRefreshing,
 };
 
