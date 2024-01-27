70c65fb940070a8833433f3e3af0bb636aad785e
AnnaFutryk <anna.futryk@gmail.com>
2024-01-27 23:21:25 +0200

    Update RestrictedRoute.js

--- src/components/RestrictedRoute.js
+++ src/components/RestrictedRoute.js
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import authSelectors from "../redux/auth/authSelectors";
 
-const RestrictedRoute = ({ children, redirectTo = "/" }) => {
+const RestrictedRoute = ({ children, redirectTo }) => {
   const isLoggedIn = useSelector(authSelectors.selectIsLoggedIn);
 
   return isLoggedIn ? <Navigate to={redirectTo} /> : children;
